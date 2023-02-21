9dac3a3c6f0d004f2d285192a1cb25b29134b780
NEWYORKHEART911 <lavillelumiere8@gmail.com>
2023-02-21 05:27:29 +0000

    changes1

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -12,6 +12,8 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        return false;
+        StringBuilder newStr = new StringBuilder(str);
+        String rev = new String(newStr.reverse());
+        return str.equals(rev);
     }
 }
